78d2df4769b694fd9665c6b7aac5aff72d93a427
Murhaf Sousli <murhafsousli@gmail.com>
2017-12-02 12:46:27 +0300

    Use axios to fetch json request

--- schema.js
+++ schema.js
@@ -7,24 +7,7 @@ const {
     GraphQLNonNull
 } = require('graphql');
 
-// Hardcoded data
-
-const customers = [{
-    id: '1',
-    name: 'John Doe',
-    email: 'jdoe@gmail.com',
-    age: 35
-}, {
-    id: '2',
-    name: 'Steve Smith',
-    email: 'steve@gmail.com',
-    age: 25
-}, {
-    id: '3',
-    name: 'Sara Williams',
-    email: 'sara@gmail.com',
-    age: 18
-}]
+const axios = require('axios');
 
 // Customer Type
 
@@ -59,13 +42,15 @@ const RootQuery = new GraphQLObjectType({
                 }
             },
             resolve(parentValue, args) {
-                return customers.find(customer => customer.id === args.id);
+                return axios.get('http://localhost:3000/customers/' + args.id)
+                    .then(res => res.data);
             }
         },
         customers: {
             type: new GraphQLList(CustomerType),
             resolve(parentValue, args) {
-                return customers;
+                return axios.get('http://localhost:3000/customers/')
+                .then(res => res.data);
             }
         }
     }
